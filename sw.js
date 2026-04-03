69f600a3a60f9d049b8ba41392ad877aec63e21b
rohanmukhtar-star <rohanmukhtar@gmail.com>
2026-04-03 18:12:43 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE = 'pitchiq-v1';
+const ASSETS = ['/', '/index.html', '/style.css', '/app.js'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});

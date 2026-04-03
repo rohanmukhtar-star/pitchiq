8bc897c02d1d8f51be47dfb6e652c74e0b39b102
rohanmukhtar-star <rohanmukhtar@gmail.com>
2026-04-03 18:50:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
-const CACHE = 'pitchiq-v1';
-const ASSETS = ['/', '/index.html', '/style.css', '/app.js'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
   );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });

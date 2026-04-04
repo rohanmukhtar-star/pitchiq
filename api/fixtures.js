986dc33dd12cc2f9a2e6b8761646b00657690cda
rohanmukhtar-star <rohanmukhtar@gmail.com>
2026-04-04 19:14:54 +0100

    Update fixtures.js

--- api/fixtures.js
+++ api/fixtures.js
@@ -5,12 +5,12 @@ export default async function handler(req, res) {
   const today = new Date().toISOString().split('T')[0];
   const future = new Date(Date.now() + 86400000 * 45).toISOString().split('T')[0];
 
-  const map = { PL:'PL', BL:'BL1', SA:'SA', PD:'PD' };
+  const map = { PL:'PL', BL:'BL1', SA:'SA', PD:'PD', FL:'FL1', DED:'DED' };
   const code = map[filter] || 'PL';
 
   try {
     const response = await fetch(
-      `https://api.football-data.org/v4/competitions/${code}/matches?dateFrom=${today}&dateTo=${future}`,
+      'https://api.football-data.org/v4/competitions/' + code + '/matches?dateFrom=' + today + '&dateTo=' + future,
       { headers: { 'X-Auth-Token': 'b89f9b0e01c546c0a4c2a36107bbd57c' } }
     );
     const data = await response.json();
